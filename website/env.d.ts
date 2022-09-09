2d7c5c6a959800f1981809e09acd082dfa6b8438
Griko Nibras <git@griko.id>
2022-09-09 10:34:37 +0700

    feat(env): add NEXT_PUBLIC_SPLITBEE_TOKEN

--- website/env.d.ts
+++ website/env.d.ts
@@ -3,6 +3,7 @@ declare namespace NodeJS {
     readonly GRAPHQL_ENDPOINT: string;
     readonly GRAPHQL_TOKEN: string;
     readonly NEXT_PUBLIC_AXIOM_INGEST_ENDPOINT: string;
+    readonly NEXT_PUBLIC_SPLITBEE_TOKEN: string;
   }
 }
 
