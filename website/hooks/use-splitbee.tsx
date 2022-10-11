cb26da3cb1e979f19edbd030676751b9086e9ed3
Griko Nibras <git@griko.id>
2022-10-12 05:27:51 +0700

    chore(hooks): allow splitbee if token exists

--- website/hooks/use-splitbee.tsx
+++ website/hooks/use-splitbee.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 
 export function useSplitbee() {
   useEffect(() => {
-    if (__PROD__ && process.env.NEXT_PUBLIC_SPLITBEE_TOKEN) {
+    if (process.env.NEXT_PUBLIC_SPLITBEE_TOKEN) {
       splitbee.init({
         apiUrl: "/_hive",
         scriptUrl: "/bee.js",
